da21fe429e72f77e559183bc6009bfc2ee9c9be2
ahmedkhafaji <four01four@gmail.com>
2022-07-28 17:20:39 -0700

    initial commit

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,44 @@
+class AnimalsController < ApplicationController
+  def index
+    animals = Animal.all
+    render json: animals
+  end
+
+  def show
+    animals = Animal.find(params[:id])
+    render json: animals
+  end
+
+  def create
+    animals = Animal.create(animals_params)
+    if animals.valid?
+      render json: animals
+    else
+      render json: animals.errors
+    end
+  end
+
+   def destroy
+      animals = Animal.find(params[:id])
+      if animals.destroy
+        render json: animals
+      else
+        render json: animals.errors
+      end
+    end
+
+    def update
+      animals = Animal.find(params[:id])
+      animals.update(animals_params)
+      if animals.valid?
+        render json: animals
+      else
+        render json: animals.errors
+      end
+    end
+
+  private
+  def animals_params
+    params.require(:animal).permit(:name, :binomial)
+  end
+end
